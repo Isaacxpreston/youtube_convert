19d309b3933204702fd0a0b147f7ad01abe52f18
Isaac <isaacxpreston@gmail.com>
2017-02-14 06:14:34 -0800

    small changes

--- client/visualiser/scripts/display_from_url.js
+++ client/visualiser/scripts/display_from_url.js
@@ -4,10 +4,12 @@ document.getElementsByTagName("video")[0].width = window.innerWidth
 axios.get('/api' + window.location.pathname)
 .then(function (response) {
   document.getElementsByTagName("video")[0].src = response.data
+  document.getElementsByTagName("video")[1].src = response.data
   initAudio()
   init()
   animate()
   document.getElementsByTagName("video")[0].play()
+  document.getElementsByTagName("video")[1].play()
 })
 .catch(function (error) {
   console.log(error);
