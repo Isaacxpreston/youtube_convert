30ef3474b556ec5f5339ffd78b9ea214425c87fc
Isaac <isaacxpreston@gmail.com>
2017-02-15 06:11:31 -0800

    reset

--- server/video_server/video_server.js
+++ server/video_server/video_server.js
@@ -31,19 +31,7 @@ var compress = zlib.createGzip();
 //   console.log(details);
 // });
 
-// const s3 = require('s3');
-// const client = s3.createClient({
-//   maxAsyncS3: 20,
-//   s3RetryCount: 3,
-//   s3RetryDelay: 1000,
-//   multipartUploadThreshold: 20971520,
-//   multipartUploadSize: 15728640,
-//   s3Options: {
-//     accessKeyId: "AKIAIPHGWAUV5F5J4AIA",
-//     secretAccessKey: "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",
-//     region: "us-west-1",
-//   },
-// });
+
 const app = express();
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
