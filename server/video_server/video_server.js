7f2efd677b05e33d9aea9c0f8b6d65a6e2935d68
Isaac <isaacxpreston@gmail.com>
2017-02-15 02:06:29 -0800

    cors allow

--- server/video_server/video_server.js
+++ server/video_server/video_server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 // APP SETUP & MIDDLEWARE
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../../client/visualiser'));
 
